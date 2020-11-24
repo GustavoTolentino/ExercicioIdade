6b19e9554e4072ad2228928b774df18e713b8175
TolentinoGustavo <tolentinogustavo@gmail.com>
2020-11-24 14:41:13 -0300

    Projeto Finalizado

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExercicioSENAI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola, Bem-Vindo ao sistema de validacao de eleitor 2020 \n Para saber se voce podera votar nessas eleicoes, insira sua idade");
+            int idade = 0;
+            idade = int.Parse(Console.ReadLine());
+
+            if(idade >= 18){
+                Console.WriteLine($"Otimo, voce ja possui {idade} anos e portanto ja pode votar!");
+            }else{
+                Console.WriteLine($"Infelizmente voce so possui {idade} anos e portanto nao podera votar nessas eleicoes");
+            }
+        }
+    }
+}
